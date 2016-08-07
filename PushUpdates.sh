60d22c5b107973b4a12b55bb221ba62be19987b4
Josh Crowley <ruvosounds@gmail.com>
2016-08-07 17:14:56 -0600

    Josh commit on client side work

--- PushUpdates.sh
+++ PushUpdates.sh
@@ -9,7 +9,6 @@ git push;
 echo ' --> Done Syncing Crowley Carpets  to GITHUB.com';
 
 
-sleep 2;
 echo ' ----> HAVING PRODUCTION SERVER Update what is live..';
 ssh ubuntu@crowleycarpets.com 'bash -s /home/ubuntu/Production/push_client_into_server.sh'
 
